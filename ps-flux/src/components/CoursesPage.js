c937f6a0fbe167ae7d9cd9fcdb505f2f1de923fe
hammad1985 <hammad.yaqoob1985@yahoo.co.uk>
2020-03-08 21:12:25 +0000

    created courses page and display in table

--- ps-flux/src/components/CoursesPage.js
+++ ps-flux/src/components/CoursesPage.js
@@ -1,8 +1,39 @@
 import React from "react";
-
+import { getCourses } from "../api/courseApi";
 class CoursesPage extends React.Component {
+  state = {
+    courses: []
+  };
+
+  componentDidMount() {
+    getCourses().then(courses => this.setState({ courses: courses }));
+  }
+
+  renderRow(course) {
+    return (
+      <tr key={course.id}>
+        <td>{course.title}</td>
+        <td>{course.authorId}</td>
+        <td>{course.category}</td>
+      </tr>
+    );
+  }
   render() {
-    return <h2>Courses</h2>;
+    return (
+      <>
+        <h2>Courses</h2>
+        <table className="table">
+          <thead>
+            <tr>
+              <th>Ttile</th>
+              <th>Author ID</th>
+              <th>Category</th>
+            </tr>
+          </thead>
+          <tbody>{this.state.courses.map(this.renderRow)}</tbody>
+        </table>
+      </>
+    );
   }
 }
 
