7739e40d987dbfd76765c0d0c6074dbe8d1713f2
hammad1985 <hammad.yaqoob1985@yahoo.co.uk>
2020-03-11 20:17:10 +0000

    changed courses page to functional component to use hooks

--- ps-flux/src/components/CoursesPage.js
+++ ps-flux/src/components/CoursesPage.js
@@ -1,40 +1,38 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { getCourses } from "../api/courseApi";
-class CoursesPage extends React.Component {
-  state = {
-    courses: []
-  };
 
-  componentDidMount() {
-    getCourses().then(courses => this.setState({ courses: courses }));
-  }
+function renderRow(course) {
+  return (
+    <tr key={course.id}>
+      <td>{course.title}</td>
+      <td>{course.authorId}</td>
+      <td>{course.category}</td>
+    </tr>
+  );
+}
 
-  renderRow(course) {
-    return (
-      <tr key={course.id}>
-        <td>{course.title}</td>
-        <td>{course.authorId}</td>
-        <td>{course.category}</td>
-      </tr>
-    );
-  }
-  render() {
-    return (
-      <>
-        <h2>Courses</h2>
-        <table className="table">
-          <thead>
-            <tr>
-              <th>Ttile</th>
-              <th>Author ID</th>
-              <th>Category</th>
-            </tr>
-          </thead>
-          <tbody>{this.state.courses.map(this.renderRow)}</tbody>
-        </table>
-      </>
-    );
-  }
+function CoursesPage() {
+  const [courses, setCourses] = useState([]);
+
+  useEffect(() => {
+    getCourses().then(_courses => setCourses(_courses));
+  }, []);
+
+  return (
+    <>
+      <h2>Courses</h2>
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Ttile</th>
+            <th>Author ID</th>
+            <th>Category</th>
+          </tr>
+        </thead>
+        <tbody>{courses.map(renderRow)}</tbody>
+      </table>
+    </>
+  );
 }
 
 export default CoursesPage;
