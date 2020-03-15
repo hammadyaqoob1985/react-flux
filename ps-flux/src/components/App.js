830efbaec58983ca997585b3f164ae418c6cc671
hammad1985 <hammad.yaqoob1985@yahoo.co.uk>
2020-03-15 17:56:19 +0000

    Encapsulated app with router. Added route for direct url call to app.js. Create link to about page. For nav component created navlinks to to urls

--- ps-flux/src/components/App.js
+++ ps-flux/src/components/App.js
@@ -3,17 +3,15 @@ import HomePage from "./HomePage";
 import AboutPage from "./AboutPage";
 import CoursesPage from "./CoursesPage";
 import Header from "./common/Header";
+import { Route } from "react-router-dom";
 
 function App() {
-  function getPage() {
-    const route = window.location.pathname;
-    if (route === "/about") return <AboutPage />;
-    if (route === "/courses") return <CoursesPage />;
-    else return <HomePage />;
-  }
   return (
     <div className="container-fluid">
-      <Header /> {getPage()}
+      <Header />
+      <Route path="/" exact component={HomePage}></Route>
+      <Route path="/courses" component={CoursesPage}></Route>
+      <Route path="/about" component={AboutPage}></Route>
     </div>
   );
 }
