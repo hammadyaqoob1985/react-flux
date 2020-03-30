b67cba1c2b27601e1c6075f8336f56b054fce614
hammad1985 <hammad.yaqoob1985@yahoo.co.uk>
2020-03-30 15:10:45 +0100

    Added in useeffect to get cpurse by slug when elements loads. Only runs when slug changes

--- ps-flux/src/components/ManageCoursePage.js
+++ ps-flux/src/components/ManageCoursePage.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import CourseForm from "./CourseForm";
 import * as courseApi from "../api/courseApi";
 import { toast } from "react-toastify";
@@ -14,6 +14,12 @@ const ManageCoursePage = props => {
     category: ""
   });
 
+  useEffect(() => {
+    const slug = props.match.params.slug;
+    if (slug) {
+      courseApi.getCourseBySlug(slug).then(_course => setCourse(_course));
+    }
+  }, [props.match.params.slug]);
   function handleChange({ target }) {
     setCourse({
       ...course,
